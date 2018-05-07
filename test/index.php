8e222b96beaf822f8bace914f376d63076908414
Fagner Silva <fagnersilva.inforprog@gmail.com>
2018-05-07 16:04:09 -0300

    atualizar

--- test/index.php
+++ test/index.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../lib_ext/autoload.php';
 use Cursocomposer\Email;
 
 $novoEmail = new Email(2, "host.sdserver144.com.br", "cursos@imperiodacoruja.com.br", "*Angra995*", "ssl", "465", "contato@imperiodacoruja.com.br", "Fagner Silva");
-$novoEmail->sendEmail("Aula de Composer", "<p>Email teste da aula de <b>Composer para o Gordin!</b></p>", "cursos@imperiodacoruja.com.br", "Fagner Silva", "fagnersilva.inforprog@gmail.com", "Fagner");
+$novoEmail->sendEmail("Estuda cabeção!", "<p>Email teste enviado <b>para o Gordin!</b> da aula de composer.</p>", "cursos@imperiodacoruja.com.br", "Vai Gordin", "jthiagosampaio@gmail.com", "Fagner");
 
 echo '<pre>';
 var_dump($novoEmail);
