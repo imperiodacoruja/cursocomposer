ff6165e1dbf3ec0ed8dc6a7d919ab002d3020848
Fagner Silva <fagnersilva.inforprog@gmail.com>
2018-05-07 11:53:25 -0300

    atualizar

--- test/index.php
+++ test/index.php
@@ -1,10 +1,10 @@
 <?php
 
-require __DIR__ . '/lib_ext/autoload.php';
+require __DIR__ . '/../lib_ext/autoload.php';
 
 use Cursocomposer\Email;
 
-$novoEmail = new Email;
+$novoEmail = new Email(2, "host.sdserver144.com.br", "cursos@imperiodacoruja.com.br", "*Angra995*", "ssl", "465", "contato@imperiodacoruja.com.br", "Fagner Silva");
 $novoEmail->sendEmail("Aula de Composer", "<p>Email teste da aula de <b>Composer para o Gordin!</b></p>", "cursos@imperiodacoruja.com.br", "Fagner Silva", "fagnersilva.inforprog@gmail.com", "Fagner");
 
 echo '<pre>';
